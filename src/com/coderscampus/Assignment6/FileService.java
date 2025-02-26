080a270c46a39f5bcdba6b7a7d19071be47c840c
Anthony L. Upchurch <156884097+upchurcha400@users.noreply.github.com>
2025-02-25 22:30:05 -0600

    Formatting the code for consistency
    Formatting issues corrected.

--- src/com/coderscampus/Assignment6/FileService.java
+++ src/com/coderscampus/Assignment6/FileService.java
@@ -1,59 +1,54 @@
 package com.coderscampus.Assignment6;
 
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-
-
-
 import java.time.YearMonth;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
-
 import java.util.List;
-
 
 public class FileService {
 
 	public List<TeslaSalesData> read(String filename) throws IOException {
 		List<TeslaSalesData> data = new ArrayList<>();
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM-yy");
-		
+
 		try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
-				reader.readLine();
-				String line;
-				
-				while ((line =reader.readLine()) != null) {
-					String[] row = line.split(",");
-					
-					try {
-						String dateString = row[0];
-						String salesString = row[1];
-					
-						YearMonth date = YearMonth.parse(dateString, formatter);
-						int sales = Integer.parseInt(salesString);
-						String model = determineModel(filename);
-						data.add(new TeslaSalesData(date, sales, model));
-					} catch (DateTimeParseException | NumberFormatException e) {
-						System.out.println("There was an error is parsing the data in line: " + line + ", due to " + e.getMessage());
-						
-					}
-				}
-		    }
-		return data;
-	}
-		private String determineModel(String filename) {
-			if (filename.contentEquals("model3")) {
-				return "Model 3";
-			} else if (filename.contains("modelS")) {
-				return "Model S";
-			} else if (filename.contains("modelX")) {
-				return "Model X";
-			} else {
-					return "ummmm, we don't know that model... awkward";
+			reader.readLine();
+			String line;
+
+			while ((line = reader.readLine()) != null) {
+				String[] row = line.split(",");
+
+				try {
+					String dateString = row[0];
+					String salesString = row[1];
+
+					YearMonth date = YearMonth.parse(dateString, formatter);
+					int sales = Integer.parseInt(salesString);
+					String model = determineModel(filename);
+					data.add(new TeslaSalesData(date, sales, model));
+				} catch (DateTimeParseException | NumberFormatException e) {
+					System.out.println(
+							"There was an error is parsing the data in line: " + line + ", due to " + e.getMessage());
+
 				}
 			}
 		}
+		return data;
+	}
 
+	private String determineModel(String filename) {
+		if (filename.contentEquals("model3")) {
+			return "Model 3";
+		} else if (filename.contains("modelS")) {
+			return "Model S";
+		} else if (filename.contains("modelX")) {
+			return "Model X";
+		} else {
+			return "ummmm, we don't know that model... awkward";
+		}
+	}
+}
--- src/com/coderscampus/Assignment6/TeslaSalesData.java
+++ src/com/coderscampus/Assignment6/TeslaSalesData.java
@@ -5,49 +5,49 @@ import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 
 public class TeslaSalesData {
-    private YearMonth dateOfSales;
-    private Integer numberOfSales;
-    private String model;
+	private YearMonth dateOfSales;
+	private Integer numberOfSales;
+	private String model;
 
-    public TeslaSalesData(YearMonth dateOfSales, Integer numberOfSales, String model) {
-        this.dateOfSales = dateOfSales;
-        this.numberOfSales = numberOfSales;
-        this.model = model;
-    }
+	public TeslaSalesData(YearMonth dateOfSales, Integer numberOfSales, String model) {
+		this.dateOfSales = dateOfSales;
+		this.numberOfSales = numberOfSales;
+		this.model = model;
+	}
 
-    private YearMonth parseDate(String dateOfSales) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yy-MMM");
+	private YearMonth parseDate(String dateOfSales) {
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yy-MMM");
 
-        try {
-            return YearMonth.parse(dateOfSales, formatter);
-        } catch (DateTimeParseException e) {
-            System.err.println("Error parsing date: " + dateOfSales);
-            e.printStackTrace();
-            return null;
-        }
-    }
+		try {
+			return YearMonth.parse(dateOfSales, formatter);
+		} catch (DateTimeParseException e) {
+			System.err.println("Error parsing date: " + dateOfSales);
+			e.printStackTrace();
+			return null;
+		}
+	}
 
-    public YearMonth getDateOfSales() {
-        return dateOfSales;
-    }
+	public YearMonth getDateOfSales() {
+		return dateOfSales;
+	}
 
-    public void setDateOfSales(String dateOfSales) {
-        this.dateOfSales = YearMonth.parse(dateOfSales);
-    }
+	public void setDateOfSales(String dateOfSales) {
+		this.dateOfSales = YearMonth.parse(dateOfSales);
+	}
 
-    public Integer getNumberOfSales() {
-        return numberOfSales;
-    }
+	public Integer getNumberOfSales() {
+		return numberOfSales;
+	}
 
-    public void setNumberOfSales(Integer numberOfSales) {
-        this.numberOfSales = numberOfSales;
-    }
+	public void setNumberOfSales(Integer numberOfSales) {
+		this.numberOfSales = numberOfSales;
+	}
 
-    public String getModel() {
-        return model;
-    }
+	public String getModel() {
+		return model;
+	}
 
-    public void setModel(String model) {
-        this.model = model;
-    }
+	public void setModel(String model) {
+		this.model = model;
+	}
 }
